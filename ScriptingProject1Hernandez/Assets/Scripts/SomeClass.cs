650602d81c121f507aef4449000bc20275555b8b
raquel2548 <rehg1234@gmail.com>
2023-04-05 14:27:50 -0700

    method overloading

---
+++ ScriptingProject1Hernandez/Assets/Scripts/SomeClass.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SomeClass
+{
+    //The first Add method has a signature of
+    //"Add(int, int)". This signature mmust be unique.
+    
+    public int Add(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+    //The second Add method has a signature of 
+    //"Add(string, strring)".Again, this must be unique.
+    
+    public string Add(string str1, string str2)
+    {
+        return str1 + str2;
+    }
+}
---
+++ ScriptingProject1Hernandez/Assets/Scripts/SomeOtherClass.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SomeOtherClass : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SomeClass myClass = new SomeClass();
+
+        //The specific Add method called will depend on
+        //the arguments pass in.
+        myClass.Add(1, 2);
+        myClass.Add("Hello", "World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
